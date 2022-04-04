cb10cf9ae16ad99b430dd75cc3dcdb933b36cf31
Ruckshan <ruckshan1024@gmail.com>
2022-04-03 18:39:42 -0700

    Only for API

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const fetch = require("node-fetch");
 
 const app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
 
@@ -14,17 +12,17 @@ app.get("/", (req, res) => {
 });
 
 // Search Page
-app.post("/", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://webapp.aopa.org/AirportsAPI/airportsautocomplete/${req.body.query}`
-    );
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+// app.post("/", async (req, res) => {
+//   try {
+//     const response = await fetch(
+//       `https://webapp.aopa.org/AirportsAPI/airportsautocomplete/${req.body.query}`
+//     );
+//     const data = await response.json();
+//     res.status(200).json(data);
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
 
 // Search Page API
 app.post("/api", async (req, res) => {
@@ -40,58 +38,58 @@ app.post("/api", async (req, res) => {
 });
 
 // Airport Page
-app.get("/:name", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://webapp.aopa.org/AirportsAPI/airports/${req.params.name}`
-    );
-    const data = await response.json();
+// app.get("/:name", async (req, res) => {
+//   try {
+//     const response = await fetch(
+//       `https://webapp.aopa.org/AirportsAPI/airports/${req.params.name}`
+//     );
+//     const data = await response.json();
 
-    let {
-      icaoId,
-      name,
-      attended,
-      city,
-      stateProvince,
-      mainPhone,
-      website,
-      reviewSummary,
-      landingSurfaces,
-      elev,
-      towerPresent,
-      airspace,
-      location,
-      timeZone,
-      businessFuels,
-      thumbNail,
-      businesses,
-    } = data;
-    console.log(data.businessFuels);
-    const sortedData = {
-      icaoId,
-      name,
-      attended,
-      city,
-      stateProvince,
-      mainPhone,
-      website,
-      reviewSummary,
-      landingSurfaces,
-      elev,
-      towerPresent,
-      airspace,
-      location,
-      timeZone,
-      businessFuels,
-      thumbNail,
-      businesses,
-    };
+//     let {
+//       icaoId,
+//       name,
+//       attended,
+//       city,
+//       stateProvince,
+//       mainPhone,
+//       website,
+//       reviewSummary,
+//       landingSurfaces,
+//       elev,
+//       towerPresent,
+//       airspace,
+//       location,
+//       timeZone,
+//       businessFuels,
+//       thumbNail,
+//       businesses,
+//     } = data;
+//     console.log(data.businessFuels);
+//     const sortedData = {
+//       icaoId,
+//       name,
+//       attended,
+//       city,
+//       stateProvince,
+//       mainPhone,
+//       website,
+//       reviewSummary,
+//       landingSurfaces,
+//       elev,
+//       towerPresent,
+//       airspace,
+//       location,
+//       timeZone,
+//       businessFuels,
+//       thumbNail,
+//       businesses,
+//     };
 
-    res.status(200).render("airport", { data: sortedData });
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+//     res.status(200).render("airport", { data: sortedData });
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
 // Airport Page API
 app.get("/api/:name", async (req, res) => {
   try {

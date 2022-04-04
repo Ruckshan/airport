f3472cc269e76ec0614c7f2e50ccaf9f7c2bfebd
Ruckshan <ruckshan1024@gmail.com>
2022-04-03 18:27:36 -0700

    Changing the API

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ app.post("/", async (req, res) => {
   }
 });
 
+// Search Page API
+app.post("/api", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://webapp.aopa.org/AirportsAPI/airportsautocomplete/${req.body.query}`
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 // Airport Page
 app.get("/:name", async (req, res) => {
   try {
@@ -51,6 +64,7 @@ app.get("/:name", async (req, res) => {
       timeZone,
       businessFuels,
       thumbNail,
+      businesses,
     } = data;
     console.log(data.businessFuels);
     const sortedData = {
@@ -70,9 +84,63 @@ app.get("/:name", async (req, res) => {
       timeZone,
       businessFuels,
       thumbNail,
+      businesses,
     };
 
     res.status(200).render("airport", { data: sortedData });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+// Airport Page API
+app.get("/api/:name", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://webapp.aopa.org/AirportsAPI/airports/${req.params.name}`
+    );
+    const data = await response.json();
+
+    let {
+      icaoId,
+      name,
+      attended,
+      city,
+      stateProvince,
+      mainPhone,
+      website,
+      reviewSummary,
+      landingSurfaces,
+      elev,
+      towerPresent,
+      airspace,
+      location,
+      timeZone,
+      businessFuels,
+      thumbNail,
+      businesses,
+    } = data;
+    console.log(data.businessFuels);
+    const sortedData = {
+      icaoId,
+      name,
+      attended,
+      city,
+      stateProvince,
+      mainPhone,
+      website,
+      reviewSummary,
+      landingSurfaces,
+      elev,
+      towerPresent,
+      airspace,
+      location,
+      timeZone,
+      businessFuels,
+      thumbNail,
+      businesses,
+    };
+
+    res.status(200).json(sortedData);
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }

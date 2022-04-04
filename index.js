d62184065a56d29edd1d361fac0e4eba20e02938
Ruckshan <ruckshan1024@gmail.com>
2022-04-03 18:49:00 -0700

    Changing the port

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 // Home Page
+// app.get("/", (req, res) => {
+//   res.status(200).render("map");
+// });
+// Home Page
 app.get("/", (req, res) => {
-  res.status(200).render("map");
+  res.status(200).Send("Airport API");
 });
 
 // Search Page
@@ -23,19 +27,6 @@ app.get("/", (req, res) => {
 //     res.status(500).json({ message: "Internal Server Error" });
 //   }
 // });
-
-// Search Page API
-app.post("/api", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://webapp.aopa.org/AirportsAPI/airportsautocomplete/${req.body.query}`
-    );
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
 
 // Airport Page
 // app.get("/:name", async (req, res) => {
@@ -90,6 +81,18 @@ app.post("/api", async (req, res) => {
 //     res.status(500).json({ message: "Internal Server Error" });
 //   }
 // });
+// Search Page API
+app.post("/api", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://webapp.aopa.org/AirportsAPI/airportsautocomplete/${req.body.query}`
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 // Airport Page API
 app.get("/api/:name", async (req, res) => {
   try {
@@ -143,7 +146,7 @@ app.get("/api/:name", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
-
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("Server Started");
 });
